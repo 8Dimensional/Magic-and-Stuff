37b20f023d14c8ee9681b68d04c6d44b7e1b6daf
alexanderlibra <Libra@DESKTOP-K99TANI>
2024-11-10 21:04:55 -0600

    Added Resin

--- src/main/java/net/mcreator/mns/init/MnsModItems.java
+++ src/main/java/net/mcreator/mns/init/MnsModItems.java
@@ -11,11 +11,13 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraft.world.item.Item;
 
 import net.mcreator.mns.item.WoodenWandItem;
+import net.mcreator.mns.item.ResinItem;
 import net.mcreator.mns.MnsMod;
 
 public class MnsModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MnsMod.MODID);
 	public static final RegistryObject<Item> WOODEN_WAND = REGISTRY.register("wooden_wand", () -> new WoodenWandItem());
+	public static final RegistryObject<Item> RESIN = REGISTRY.register("resin", () -> new ResinItem());
 	// Start of user code block custom items
 	// End of user code block custom items
 }
