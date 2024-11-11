bba0ef2b3d687d1d8d4e6d84b19fe523f2a56b60
8Dimensional <Trey@TreysDesktop>
2024-11-10 21:18:56 -0600

    Added Spell Tome

--- src/main/java/net/mcreator/mns/init/MnsModItems.java
+++ src/main/java/net/mcreator/mns/init/MnsModItems.java
@@ -11,6 +11,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraft.world.item.Item;
 
 import net.mcreator.mns.item.WoodenWandItem;
+import net.mcreator.mns.item.SpellTomeItem;
 import net.mcreator.mns.item.ResinItem;
 import net.mcreator.mns.MnsMod;
 
@@ -18,6 +19,7 @@ public class MnsModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MnsMod.MODID);
 	public static final RegistryObject<Item> WOODEN_WAND = REGISTRY.register("wooden_wand", () -> new WoodenWandItem());
 	public static final RegistryObject<Item> RESIN = REGISTRY.register("resin", () -> new ResinItem());
+	public static final RegistryObject<Item> SPELL_TOME = REGISTRY.register("spell_tome", () -> new SpellTomeItem());
 	// Start of user code block custom items
 	// End of user code block custom items
 }
