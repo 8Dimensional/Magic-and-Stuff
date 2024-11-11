54e98869c36e13097a55d0dedf122622081b4312
8Dimensional <Trey@TreysDesktop>
2024-11-10 21:26:17 -0600

    Updated code I guess?

--- src/main/java/net/mcreator/mns/item/SpellTomeItem.java
+++ src/main/java/net/mcreator/mns/item/SpellTomeItem.java
@@ -1,6 +1,24 @@
 
 package net.mcreator.mns.item;
 
+import net.minecraftforge.registries.ForgeRegistries;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.projectile.SpectralArrow;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.resources.ResourceLocation;
+
 public class SpellTomeItem extends Item {
 	public SpellTomeItem() {
 		super(new Item.Properties().stacksTo(1).fireResistant().rarity(Rarity.EPIC));
