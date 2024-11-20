713c4f05f0b34ffa8317754eb87a6f27d056141c
Trey <Trey@TreysDesktop>
2024-11-20 16:04:10 -0600

    MCreator Merge commit 'c2514b43245088e031561f00da0f2d3f0cecf90c' into fb620eeb58e480dedb57374ba87d7d96e0b24114

--- src/main/java/net/mcreator/mns/init/MnsModItems.java
+++ src/main/java/net/mcreator/mns/init/MnsModItems.java
@@ -19,8 +19,8 @@ import net.mcreator.mns.MnsMod;
 public class MnsModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MnsMod.MODID);
 	public static final RegistryObject<Item> RESIN = REGISTRY.register("resin", () -> new ResinItem());
-	public static final RegistryObject<Item> TATTERED_SPELL_TOME = REGISTRY.register("tattered_spell_tome", () -> new TatteredSpellTomeItem());
 	public static final RegistryObject<Item> CROOKED_WAND = REGISTRY.register("crooked_wand", () -> new CrookedWandItem());
+	public static final RegistryObject<Item> TATTERED_SPELL_TOME = REGISTRY.register("tattered_spell_tome", () -> new TatteredSpellTomeItem());
 	public static final RegistryObject<Item> GUIDE_SCROLL = REGISTRY.register("guide_scroll", () -> new GuideScrollItem());
 	// Start of user code block custom items
 	// End of user code block custom items
